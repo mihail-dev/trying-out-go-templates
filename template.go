67e26f91bc70115a6662a1ead2c6d1654a786ae9
mihail-dev <mihail.ivanov@accenture.com>
2016-10-28 11:45:38 +0000

    Trying out if

--- template.go
+++ template.go
@@ -1,3 +1,14 @@
 package main
 
-var nginxConfTemplate = `{{.Count}} items are made of {{.Material}}\n`
+var nginxConfTemplate = `{{.Count}} items are made of {{.Material}}
+
+{{if eq "gerrit" "gerrit" }} blablabla {{end}}
+
+{{ if false }} don't write this {{ end }} 
+
+
+
+
+
+
+`
