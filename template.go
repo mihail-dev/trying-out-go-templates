7229727d798bca9be6f2459d92bee4820b0b3451
mihail-dev <mihail.ivanov@accenture.com>
2016-10-28 12:19:25 +0000

    Else

--- template.go
+++ template.go
@@ -2,7 +2,11 @@ package main
 
 var nginxConfTemplate = `{{.Count}} items are made of {{.Material}}
 
-{{if eq "gerrit" "gerrit" }} blablabla {{end}}
+{{if eq "jenkins" "gerrit" }} 
+blablabla 
+{{ else if true }}
+some other text 
+{{end}}
 
 {{ if false }} don't write this {{ end }} 
 
